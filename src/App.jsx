7b0a523b7d378fe27bdedf8af77eed4edee77847
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-05 01:25:50 +0300

    Reworking the filles for Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import info from "./components/info";
+import about from "./components/about";
+import interest from "./components/interest";
+import footer from "./components/footer";
 
+export default function App() {
+  return (
+    <div className="card-wrapper">
+      <info /> {/* Picture, Name, Buttons */}
+      <about /> {/* Biography Paragraph */}
+      <interests /> {/* Hobbies / Skills */}
+      <footer /> {/* Social media footer links */}
+    </div>
+  );
+}
