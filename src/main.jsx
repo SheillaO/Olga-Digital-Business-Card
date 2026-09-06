212dfb2dc429e36b82086f32efcfe9bddc5293ee
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-06 17:23:32 +0300

    Remove decorative comments & tidy formatting
    Remove decorative comment separators and unused inline comments from src/index.css and src/main.jsx, plus minor whitespace cleanup. Purely cosmetic formatting changes across those files; no functional or styling behavior was altered.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // <-- It imports your main App here!
-import "./index.css"; // <-- It loads your styles here!
+import App from "./App.jsx"; 
+import "./index.css"; 
 
-// This is the bridge that mounts your React app to the HTML
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
