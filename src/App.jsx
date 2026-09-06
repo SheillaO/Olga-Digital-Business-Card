0d3deb267f55ffe8f0dc5d1770c5998d8dd60e66
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-06 16:39:40 +0300

    Reworking the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Info from "./Components/Info";
 import About from "./Components/About";
-import Interests from "./Components/Interests";
+import Interest from "./Components/Interest";
 import Footer from "./Components/Footer";
 
 export default function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./Components/App.jsx"; // <-- It imports your main App here!
+import App from "./App.jsx"; // <-- It imports your main App here!
 import "./index.css"; // <-- It loads your styles here!
 
 // This is the bridge that mounts your React app to the HTML
